f412c66641b414fdad28d562a5187c0adc9ff8bb
ATrejo <anghelardon2015@gmail.com>
2024-07-28 13:49:17 -0600

    - Se habilito funcion de eliminar

--- CrudFirebase/Views/ListProducto.xaml.cs
+++ CrudFirebase/Views/ListProducto.xaml.cs
@@ -1,9 +1,28 @@
+using CrudFirebase.Models;
+using CrudFirebase.ViewModels;
+
 namespace CrudFirebase.Views;
 
 public partial class ListProducto : ContentPage
 {
-	public ListProducto()
-	{
-		InitializeComponent();
-	}
+    public ListProducto()
+    {
+        InitializeComponent();
+        BindingContext = new ProductosViewModel(); // Asegúrate de que el ViewModel está establecido como BindingContext
+    }
+
+    private async void OnDeleteProducto(object sender, EventArgs e)
+    {
+        var menuItem = sender as MenuItem;
+        var producto = menuItem?.CommandParameter as productosModel;
+
+        if (producto != null)
+        {
+            var viewModel = BindingContext as ProductosViewModel;
+            if (viewModel != null)
+            {
+                viewModel.DeleteProductoCommand.Execute(producto.Id);
+            }
+        }
+    }
 }
